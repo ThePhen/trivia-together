4d7521e709323f13440331bd733f603a6d07d1ac
Stephen Henderson <stephenhenderson8@gmail.com>
2025-06-18 14:04:22 -0500

    docker-ize the default app

--- frontend-client-react/src/app/page.tsx
+++ frontend-client-react/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
+          <li className="tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               src/app/page.tsx
@@ -23,8 +23,19 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            Learn more about{" "}
+            <a
+              className="text-blue-500 hover:underline"
+              href="https://nextjs.org/docs/app/building-your-application/routing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              routing in Next.js
+            </a>
+            .
+          </li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
